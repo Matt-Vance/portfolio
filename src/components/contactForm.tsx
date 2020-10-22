ab6cdb1d646b151c8623c1f5b8ef4af9cbafd8a7
Matt Vance <mvance43776@gmail.com>
2020-10-21 23:20:01 -0700

    Copied form submission example

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,17 +1,55 @@
 import * as React from "react";
+import { navigate } from 'gatsby-link'
 
-function ContactForm(){
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
+function ContactForm() {
+  const [formValues, setFormValues] = React.useState({});
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...formValues, // your fields paired with key: name
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch((error) => alert(error));
+  };
   return (
     <form
       className="contact-form"
       name="contact"
-      method="POST"
+      method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
+      onSubmit={handleSubmit}
       action="/"
     >
       <input type="hidden" name="bot-field" />
-      <input type="hidden" name="form-name" value="contact" />
+      <p hidden>
+        <label>
+          Don’t fill this out:
+          <input name="bot-field" onChange={handleChange} />
+        </label>
+      </p>
+      <input
+        type="hidden"
+        name="form-name"
+        value="contact"
+        onChange={handleChange}
+      />
       <div className="form-field">
         <input
           type="text"
@@ -30,12 +68,17 @@ function ContactForm(){
           className="input-text"
           name="email"
           placeholder="enter your email"
+          onChange={handleChange}
           required
         />
         <label htmlFor="email">email </label>
       </div>
-      <div className="form-field" id = "message-text">
-        <textarea className="input-text" name="message"></textarea>
+      <div className="form-field" id="message-text">
+        <textarea
+          className="input-text"
+          name="message"
+          onChange={handleChange}
+        ></textarea>
         <label>message: </label>
       </div>
 
